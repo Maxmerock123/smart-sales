c27d9c06a72f09df37b4e84209f7a78f32cbfd3b
Maxmerock123 <Maxmerock@gmail.com>
2023-12-11 11:46:50 +0700

    add everypage and route for it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import GeneralForm from './components/GeneralForm'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormPage from './pages/FormPage'
+import ServicesPage from './pages/ServicesPage'
+import MenuPage from './pages/MenuPage'
+import PackagePage from './pages/PackagePage'
+import SummaryPage from './pages/SummaryPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +20,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<FormPage />} />
+          <Route path='/ServicesPage' element={<ServicesPage />} />
+          <Route path='/PackagePage' element={<PackagePage />} />
+          <Route path='/MenuPage' element={<MenuPage />} />
+          <Route path='/SummaryPage' element={<SummaryPage />} />
         </Routes>
       </BrowserRouter>
     </>
