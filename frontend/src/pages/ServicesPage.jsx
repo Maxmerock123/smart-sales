c9620b567366da49fe2bb89e40f00e87591b0693
Maxmerock123 <Maxmerock@gmail.com>
2023-12-12 15:06:54 +0700

    fix basic element and card to ServicesPage

--- frontend/src/pages/ServicesPage.jsx
+++ frontend/src/pages/ServicesPage.jsx
@@ -3,15 +3,32 @@ import Header from '../components/Header'
 import style from './ServicesPage.module.css'
 
 const ServicesPage = () => {
-  return (
-   <>
-    <Header/>
-    <div>
-      <div className={style.cardContainer}>
 
+
+  return (
+    <>
+      <Header />
+      <div>
+        <div className={style.cardContainer}>
+          <div className={style.card}>
+            <div className={style.mockImg}>image</div>
+            <p>lorem ipsum, nason major</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.mockImg}>image</div>
+            <p>lorem ipsum, nason major</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.mockImg}>image</div>
+            <p>lorem ipsum, nason major</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.mockImg}>image</div>
+            <p>lorem ipsum, nason major</p>
+          </div>
+        </div>
       </div>
-    </div>
-   </>
+    </>
   )
 }
 
