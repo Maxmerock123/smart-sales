0bb14dfbc93d0187296fd743917a3bcb8970adf9
Maxmerock123 <Maxmerock@gmail.com>
2023-12-13 11:13:25 +0700

    add navigate tp -1 when click back button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,14 +3,20 @@ import style from './Header.module.css'
 import logo from '../../public/impact-logo.png'
 import { IoIosArrowBack } from "react-icons/io";
 import { PiPhoneCallFill } from "react-icons/pi";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate()
+    const onBackHandler = () => {
+        navigate(-1)
+    }
+
     return (
         <>
             <div className='flex items-center justify-center gap-2 h-28 bg-gradient-to-b from-custom-yellow to-transparent' >
                 <div className='mt-24 mb-8 flex justify-center items-center gap-[50px]'>
 
-                    <IoIosArrowBack size={"40px"} />
+                    <IoIosArrowBack size={"40px"} onClick={onBackHandler} />
                     <img className='w-[150px] ' src={logo}></img>
                     <PiPhoneCallFill size={"40px"} />
                 </div>
