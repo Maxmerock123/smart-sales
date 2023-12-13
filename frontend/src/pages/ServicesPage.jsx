601b084884e020ad8ef730f3803deb11a23ad508
Maxmerock123 <Maxmerock@gmail.com>
2023-12-13 14:07:18 +0700

    map for items to add card in ServicesPage

--- frontend/src/pages/ServicesPage.jsx
+++ frontend/src/pages/ServicesPage.jsx
@@ -2,32 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 import style from './ServicesPage.module.css';
 import '../index.css';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const ServicesPage = () => {
+  const items = [
+    { "title": "บริการที่ 1", "link": "/PackagePage" },
+    { "title": "บริการที่ 2" },
+    { "title": "บริการที่ 3" },
+    { "title": "บริการที่ 4" },
+  ]
+
+  const navigate = useNavigate()
+  function onClickHandler() {
+    console.log("clicked");
+    navigate(items[0].link)
+  }
+
+
 
 
   return (
     <>
-      <Header />
-      <div>
-        <h3 className='text-3xl font-bold underline'>เลือกประเภทงาน</h3>
-        <div className={style.cardContainer}>
-          <div className={style.card}>
-            <div className={style.mockImg}>image</div>
-            <p>lorem ipsum, nason major</p>
+      <div className=''>
+        <Header />
+        <div className='flex flex-col items-center gap-5'>
+          <h3 className='m-8 font-bold text-center'>เลือกประเภทงาน</h3>
+
+          {/* cardContainer */}
+          <div className='flex w-[100vw] gap-5 flex-wrap justify-center'>
+            {items.map((item) => (
+              <div className='flex flex-wrap  justify-center w-[150px] '>
+                <div className='bg-slate-400 w-[150px] aspect-square rounded-[15px] text-center ' onClick={onClickHandler} >IMAGE</div>
+                <p>{item.title}</p>
+              </div>
+            ))}
           </div>
-          <div className={style.card}>
-            <div className={style.mockImg}>image</div>
-            <p>lorem ipsum, nason major</p>
-          </div>
-          <div className={style.card}>
-            <div className={style.mockImg}>image</div>
-            <p>lorem ipsum, nason major</p>
-          </div>
-          <div className={style.card}>
-            <div className={style.mockImg}>image</div>
-            <p>lorem ipsum, nason major</p>
-          </div>
+
+
         </div>
       </div>
     </>
