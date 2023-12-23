fe02c64c33a28b6f97ce4fbd4c677ed958995b38
Maxmerock123 <Maxmerock@gmail.com>
2023-12-23 20:59:04 +0700

    change plain insideMeeting uisng ItemCard Component

--- frontend/src/pages/inside/InsideMeeting.jsx
+++ frontend/src/pages/inside/InsideMeeting.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { useNavigate,Navigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 import FormDataContext from '../../FormDataContext';
 import chineseTableImg from '../../img/food/chineseTable.jpg'
 import cocktailImg from '../../img/food/cocktail.jpg'
@@ -11,47 +11,32 @@ import CardItem from '../../components/CardItem';
 const InsideMeeting = () => {
     const navigate = useNavigate()
     const { formData, setFormData } = useContext(FormDataContext);
-    
-    
-    
+
+
+
     function onClickHandler(e) {
         console.log(`clicked on ${e.target.id}`)
         setFormData({
-            ...formData,eventForm: {
+            ...formData, eventForm: {
                 ...formData.eventForm,
                 eventType: e.target.id
             }
-        })    
+        })
         navigate('/SummaryPage')
-        
+
     }
 
     return (
         <>
             <h3 className='m-8 font-bold text-center'>เลือกประเภทอาหาร</h3>
             <div className='flex flex-wrap justify-center gap-10 mt-10'>
-                <div className='flex flex-col items-center'>
-                    <img src={coffeeBreakImg} id='outsideCoffee' className='w-[130px] aspect-square object-cover object-top bg-gray-300 rounded-[15px]' onClick={onClickHandler} />
-                    <p>คอฟฟี่เบรค</p>
-                </div>
-                <div className='flex flex-col items-center'>
-                    <img src={thaiBuffetImg} id='outsideThaiBuffet' className='object-cover w-[130px] aspect-square bg-gray-300 rounded-[15px]' onClick={onClickHandler} />
-                    <p>ไทยบุฟเฟ่ต์</p>
-                </div>
-                <div className='flex flex-col items-center'>
-                    <img src={cocktailImg} id='outsideCocktail' className='object-cover w-[130px] aspect-square bg-gray-300 rounded-[15px]' onClick={onClickHandler} />
-                    <p>ค็อกเทล</p>
-                </div>
-                <div className='flex flex-col items-center'>
-                    <img src={westernSetImg} id='outsideWesternSet' className='object-cover w-[130px] aspect-square bg-gray-300 rounded-[15px]' onClick={onClickHandler} />
-                    <p>อาหารนานาชาติ</p>
-                </div>
-                <div className='flex flex-col items-center'>
-                    <img src={chineseTableImg} id='outsideChineseTable' className='object-cover w-[130px] aspect-square bg-gray-300 rounded-[15px]'/>
-                    <p>โต๊ะจีน (10 ท่าน)</p>
-                </div>
-                <CardItem url="#" price='12dollar' body='lorem ipsum'/>
+                <CardItem id='outsideCoffee' url={coffeeBreakImg} body='คอฟฟี่เบรค' onClick={onClickHandler} />
+                <CardItem id='outsideThaiBuffet' url={thaiBuffetImg} body='ไทยบุฟเฟ่ต์' onClick={onClickHandler} />
+                <CardItem id='outsideCocktail' url={cocktailImg} body='ค็อกเทล' onClick={onClickHandler} />
+                <CardItem id='outsideWesternSet' url={westernSetImg} body='อาหารนานาชาติ' onClick={onClickHandler} />
+                <CardItem id='outsideChineseTable' url={chineseTableImg} body='โต๊ะจีน (10 ท่าน)' onClick={onClickHandler} />
             </div>
+            <CardItem url="#" price='12dollar' body='lorem ipsum' />
         </>
     )
 }
