55b72168f31184732151e84d0abd0f4d90b4c5c0
Maxmerock123 <Maxmerock@gmail.com>
2023-12-24 12:17:49 +0700

    add img to InsideParty page

--- frontend/src/pages/inside/InsideParty.jsx
+++ frontend/src/pages/inside/InsideParty.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import privateParty from '../../img/food/privateParty.jpg'
 import CardItem from '../../components/CardItem'
+import thaiBuffet from '../../img/inside/thai-chinese-table.jpg'
+import internationalBuffet from '../../img/inside/international-buffet.jpg'
+import chineseTable from '../../img/inside/chinese-table.png'
+import cocktail from '../../img/food/cocktail.jpg'
+
 
 const InsideParty = () => {
     function onClickHandler() {
@@ -11,10 +15,10 @@ const InsideParty = () => {
         <>
             <h3 className='m-8 font-bold text-center'>เลือกแพ็คเกจ</h3>
             <div className='flex flex-wrap justify-center gap-10 mt-10'>
-                <CardItem id='outsideCoffee' url='#' price='#' body='ไทยบุฟเฟต์ {hide 700}' onClick={onClickHandler} />
-                <CardItem id='outsideCoffee' url='#' price='#' body='ค็อกเทล {hide 720}' onClick={onClickHandler} />
-                <CardItem id='outsideCoffee' url='#' price='#' body='บุฟเฟต์อาหารไทยและเวสเทิร์น {hide 770}' onClick={onClickHandler} />
-                <CardItem id='outsideCoffee' url='#' price='#' body='โต๊ะจีน (สำหรับ 10 ท่าน) {hide 8900}' onClick={onClickHandler} />
+                <CardItem id='outsideCoffee' url={thaiBuffet} price='#' body='ไทยบุฟเฟต์ {hide 700}' onClick={onClickHandler} />
+                <CardItem id='outsideCoffee' url={cocktail} price='#' body='ค็อกเทล {hide 720}' onClick={onClickHandler} />
+                <CardItem id='outsideCoffee' url={internationalBuffet} price='#' body='บุฟเฟต์อาหารไทยและเวสเทิร์น {hide 770}' onClick={onClickHandler} />
+                <CardItem id='outsideCoffee' url={chineseTable} price='#' body='โต๊ะจีน (สำหรับ 10 ท่าน) {hide 8900}' onClick={onClickHandler} />
             </div>
         </>)
 }
