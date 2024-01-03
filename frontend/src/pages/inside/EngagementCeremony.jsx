36bdae93913bfacb3a3a2449d65542099d7a6b65
Maxmerock123 <Maxmerock@gmail.com>
2024-01-03 09:50:57 +0700

    add EnagementCeremony page element

--- frontend/src/pages/inside/EngagementCeremony.jsx
+++ frontend/src/pages/inside/EngagementCeremony.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CardItem from "../../components/CardItem";
 
 const EngagementCeremony = () => {
   return (
-    <div>EngagementCeremony</div>
-  )
-}
+    <>
+      <h3 className="m-8 text-center font-bold">เลือก Package</h3>
+      <div className="flex justify-center">
+        <div className="mt-10 flex w-[80%] flex-wrap justify-between gap-10">
+          <CardItem id="engage1" body="พิธีหมั้น + ยกน้ำชา" />
+          <CardItem
+            id="engage2"
+            body="พิธีหมั้น + ยกน้ำชา + หลั่งน้ำพระพุทธมนต์"
+          />
+          <CardItem
+            id="engage3"
+            body="พิธีหมั้น + ยกน้ำชา + หลั่งน้ำพระพุทธมนต์ + พิธีสงฆ์"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default EngagementCeremony
+export default EngagementCeremony;
--- frontend/src/pages/inside/WeddingCeremony.jsx
+++ frontend/src/pages/inside/WeddingCeremony.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const WeddingCeremony = () => {
   return (
-    <div>WeddingCeremony</div>
+    <>
+      <h3 className="m-8 font-bold text-center">เลือก Package</h3>
+      <div className="flex justify-center">
+        <div className="mt-10 flex w-[80%] flex-wrap justify-between gap-10">
+          <CardItem id="engage1" body="พิธีหมั้น + ยกน้ำชา" />
+          <CardItem
+            id="engage2"
+            body="พิธีหมั้น + ยกน้ำชา + หลั่งน้ำพระพุทธมนต์"
+          />
+          <CardItem
+            id="engage3"
+            body="พิธีหมั้น + ยกน้ำชา + หลั่งน้ำพระพุทธมนต์ + พิธีสงฆ์"
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
