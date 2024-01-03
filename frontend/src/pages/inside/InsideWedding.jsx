ba658bb4b9a459b4edd0fd68e521b81874d5526d
Maxmerock123 <Maxmerock@gmail.com>
2024-01-03 09:17:43 +0700

    Add new images for inside wedding page

--- frontend/src/pages/inside/InsideWedding.jsx
+++ frontend/src/pages/inside/InsideWedding.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import wedding1 from '../../img/outside/wedding/wedding1.jpg'
-import wedding2 from '../../img/outside/wedding/wedding2.jpg'
-import wedding3 from '../../img/outside/wedding/wedding3.jpg'
-import wedding4 from '../../img/outside/wedding/wedding4.jpg'
+
 import CardItem from '../../components/CardItem'
+import wedding0 from '../../img/inside/wedding0.jpg'
+import engage0 from '../../img/inside/engage0.jpg'
 
 const InsideWedding = () => {
     const navigate = useNavigate() 
@@ -18,8 +17,8 @@ const InsideWedding = () => {
         <>
             <h3 className='m-8 font-bold text-center'>เลือกแพ็คเกจ</h3>
             <div className='flex flex-wrap justify-center gap-10 mt-10'>
-                <CardItem url='#' body='พิธีหมั้น' id='EngagementCeremony' onClick={onClickHandler}/>
-                <CardItem url='#' body='งานเลี้ยง ฉลองมงคลสมรส' id='WeddingCeremony' onClick={onClickHandler}/>
+                <CardItem url={engage0} body='พิธีหมั้น' id='EngagementCeremony' onClick={onClickHandler}/>
+                <CardItem url={wedding0} body='งานเลี้ยง ฉลองมงคลสมรส' id='WeddingCeremony' onClick={onClickHandler}/>
             </div>
         </>
     )
