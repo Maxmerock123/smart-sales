f1eb5a6eefd52aebec931acccdff73888af8dc1d
Maxmerock123 <Maxmerock@gmail.com>
2024-01-04 10:10:46 +0700

    fix summaryPage

--- frontend/src/pages/SummaryPage.jsx
+++ frontend/src/pages/SummaryPage.jsx
@@ -1,29 +1,60 @@
-import React, { useContext } from 'react';
-import FormDataContext from '../FormDataContext';
+import React, { useContext } from "react";
+import FormDataContext from "../FormDataContext";
 
 const SummaryPage = () => {
   const { formData } = useContext(FormDataContext);
+  function onClickHandler() {
+    console.log("clicked sent to sales");
+  }
 
   return (
-    <div className="flex items-center justify-center ">
-      <div className=" p-3 bg-white rounded-lg shadow w-[300px]">
+    <div className="flex flex-col items-center justify-center gap-5">
+      <div className="w-[350px] rounded-lg bg-white p-3 shadow">
         <h2 className="m-3 text-2xl font-bold">ใบสรุปรายการ</h2>
-        <div className="">
-          <h3 className="font-semibold">General Form</h3>
-          <p>Name: {formData.generalForm.name}</p>
-          <p>Number: {formData.generalForm.number}</p>
-          <p>Email: {formData.generalForm.email}</p>
-        </div>
-        <div>
-          <h3 className="mb-2 font-semibold">Event Form</h3>
-          <p>Place: {formData.generalForm.place}</p>
-          <p>People: {formData.generalForm.people}</p>
-          <p>Date: {formData.generalForm.date}</p>
-          <p>EventType: { formData.generalForm.eventType }</p>
-        </div>
+        <table className="table-auto ">
+          <tbody className="">
+            <tr>
+              <td className="px-2 py-2 font-bold">ชื่อ-นามสกุล:</td>
+              <td className="px-2 py-2">{formData.generalForm.name}</td>
+            </tr>
+            <tr>
+              <td className="px-2 py-2 font-bold">เบอร์โทรศัพท์:</td>
+              <td className="px-2 py-2">{formData.generalForm.number}</td>
+            </tr>
+            <tr>
+              <td className="px-2 py-2 font-bold">E-mail:</td>
+              <td className="px-2 py-2">{formData.generalForm.email}</td>
+            </tr>
+            <tr>
+              <td className="px-2 py-2 font-bold">จำนวนแขก:</td>
+              <td className="px-2 py-2">{formData.generalForm.people}</td>
+            </tr>
+            <tr>
+              <td className="px-2 py-2 font-bold">วันที่จัด:</td>
+              <td className="px-2 py-2">{formData.generalForm.date}</td>
+            </tr>
+            <tr>
+              <td className="px-2 py-2 font-bold">ประเภทของงาน:</td>
+              <td className="px-2 py-2">{formData.generalForm.eventType}</td>
+            </tr>
+            <br></br>
+            <tr>
+              <td className="px-2 py-2 font-bold ">ราคาโดยประมาณ:</td>
+              <td className="px-2 py-2"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="flex w-[350px] justify-end">
+        <button
+          className="rounded-[15px] bg-custom-brown px-5 text-white shadow-2xl"
+          onClick={onClickHandler}
+        >
+          ส่งข้อมูลให้ฝ่ายขาย
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default SummaryPage;
