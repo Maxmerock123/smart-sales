2112347d2b385a24a04b7c7d0234ff024cbd03a4
Maxmerock123 <Maxmerock@gmail.com>
2024-02-14 11:17:07 +0700

    add images for concert page

--- frontend/src/pages/inside/Concert.jsx
+++ frontend/src/pages/inside/Concert.jsx
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import CardItem from "../../components/CardItem";
 import logo from "../../public/impact-logo.png";
+import thunderdomeImg from "../../img/inside/new-icon/inside-concert-thunderdome.jpg";
+import arenaImg from "../../img/inside/new-icon/inside-concert-arena.jpg";
+import iecImg from "../../img/inside/new-icon/inside-concert-iec.jpg";
+import challengerImg from "../../img/inside/new-icon/inside-concert-challenger.jpg";
 
 const Concert = () => {
   return (
@@ -10,28 +14,28 @@ const Concert = () => {
         <div className="mt-3 flex w-[80%] flex-wrap justify-between gap-5">
           <CardItem
             id="Concert1"
-            img={logo}
+            img={thunderdomeImg}
             url={"/summaryPage"}
             body="Thunderdome"
             dataType={"concert"}
           />
           <CardItem
             id="Concert2"
-            img={logo}
+            img={iecImg}
             url={"/summaryPage"}
             body="IEC"
             dataType={"concert"}
           />
           <CardItem
             id="Concert3"
-            img={logo}
+            img={arenaImg}
             url={"/summaryPage"}
             body="Arena"
             dataType={"concert"}
           />
           <CardItem
             id="Concert4"
-            img={logo}
+            img={challengerImg}
             url={"/summaryPage"}
             body="Challenger"
             dataType={"concert"}
