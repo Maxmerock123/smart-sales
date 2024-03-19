4b93d5af181580c7e0ec117c8e0418a5d76c503b
Maxmerock123 <Maxmerock@gmail.com>
2024-03-19 09:45:10 +0700

    add placeholder in input box

--- frontend/src/components/GeneralForm.jsx
+++ frontend/src/components/GeneralForm.jsx
@@ -77,33 +77,33 @@ const GeneralForm = () => {
 
       {showModal ? (
         <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-            <div className="relative mx-auto my-6 w-auto max-w-3xl">
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+            <div className="relative w-auto max-w-3xl mx-auto my-6">
               {/*content*/}
               <div className="relative flex w-full animate-fade-down flex-col rounded-[15px] border-0 bg-white shadow-lg outline-none focus:outline-none">
                 {/*header*/}
-                <div className="border-blueGray-200 flex items-start justify-between rounded-t border-b border-solid p-5">
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
                   <h3 className="text-3xl font-semibold">โปรดกรอกข้อมูล</h3>
                   <button
-                    className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
+                    className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
+                    <span className="block w-6 h-6 text-2xl text-black bg-transparent outline-none opacity-5 focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
                 {/*body*/}
                 <div className="relative flex-auto p-6">
-                  <p className="text-blueGray-500 my-4 text-lg leading-relaxed">
+                  <p className="my-4 text-lg leading-relaxed text-blueGray-500">
                     โปรดกรอกชื่อ ,เบอร์โทร (เลข 10 หลัก), จำนวนแขก
                     ให้ครบถ้วนและถูกต้อง แล้วเลือกถัดไปค่ะ
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="border-blueGray-200 flex items-center justify-end rounded-b border-t border-solid p-6">
+                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
                   <button
-                    className="mb-1 mr-1 rounded bg-custom-brown px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                    className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-custom-brown hover:shadow-lg focus:outline-none active:bg-emerald-600"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
@@ -119,7 +119,7 @@ const GeneralForm = () => {
 
       {/* Content */}
       <div className="m-5 flex w-[350px] animate-fade-down justify-center rounded-[15px] bg-white p-10 shadow-[0_8px_30px_rgb(0,0,0,0.12)] animate-once animate-ease-out">
-        <form className="flex w-full animate-fade-down flex-col gap-5 animate-delay-300 animate-once animate-ease-out">
+        <form className="flex flex-col w-full gap-5 animate-fade-down animate-delay-300 animate-once animate-ease-out">
           <div>
             <label className="text-black" htmlFor="name">
               ชื่อ-นามสกุล / ชื่อบริษัท
@@ -129,6 +129,7 @@ const GeneralForm = () => {
             <input
               className="h-8 min-w-[250px] rounded-[15px]  border-[1px] border-stone-300 shadow-2xl"
               type="text"
+              placeholder="ชื่อ-นามสกุล / ชื่อบริษัท"
               id="name"
               name="name"
               value={formData.generalForm.name}
@@ -148,6 +149,7 @@ const GeneralForm = () => {
               type="number"
               id="number"
               name="number"
+              placeholder="ตัวเลข 10 หลัก"
               value={formData.generalForm.number}
               onChange={handleChange}
             />
@@ -160,7 +162,8 @@ const GeneralForm = () => {
             <br />
             <input
               className="h-8 min-w-[250px] rounded-[15px]  border-[1px] border-stone-300 shadow-2xl"
-              type="text"
+              type="email"
+              placeholder="ex. name@mymail.com"
               id="email"
               name="email"
               value={formData.generalForm.email}
@@ -214,7 +217,7 @@ const GeneralForm = () => {
             </div>
           </div>
           <div>
-            <div className="m-5 flex justify-end gap-10">
+            <div className="flex justify-end gap-10 m-5">
               <button
                 className="h-[50px] w-[100px] rounded-[15px] bg-custom-brown text-white shadow-2xl"
                 onClick={onSkipHandler}
